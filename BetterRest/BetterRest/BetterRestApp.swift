d861f786f4bbbb4174d83227c281efe0ed7db462
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-05 23:09:27 +0300

    feat: initialize BetterRest and add challanges

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Oğulcan Baytimur on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
