0e41efcbb2453e70b7341d94370a8d607337d397
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-09 23:48:23 +0300

    feat: add questionView

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -12,11 +12,12 @@ struct ContentView: View {
     @State private var uptoNumber = 1
     @State private var totalNumber = 1
     @State private var questionArray: [ [Int] ] = Array()
+    @State private var answerArr: [Int] = Array()
     
     var body: some View {
-        
         VStack {
             settingView
+            questionView
         }
         .padding()
     }
@@ -30,33 +31,47 @@ extension ContentView {
     private var settingView: some View {
         VStack {
             HStack {
-                Text("Upto which number do you want to study")
+                Text("Upto which number do you want to study?")
                 Spacer()
-                Picker("Select first number", selection: $uptoNumber) {
+                Picker("Select number", selection: $uptoNumber) {
                     ForEach(0..<13) {
                         Text("\($0)")
                     }
                 }
             }
             HStack {
-                Text("Second Multiplicant")
+                Text("Numer of total question")
                 Spacer()
-                Picker("Select first number", selection: $totalNumber) {
-                    ForEach(0..<13) {
-                        Text("\($0)")
+                Picker("Select question number", selection: $totalNumber) {
+                    ForEach(1..<5) {
+                        Text("\($0*5)")
                     }
                 }
             }
+            Text("Study numbers: \(uptoNumber) and total questions \(totalNumber)")
         }
     }
-}
-
-extension ContentView {
     private var questionView: some View {
-        VStack {
-        
+        questionArray = generateQuestions()
+        answerArr.reserveCapacity(questionArray.capacity)
+        ForEach(questionArray, id: \.self) { question in
+            HStack {
+                Text("\(question[0]) x \(question[1])")
+            }
+            .padding()
+            .frame(width: 120, height: 30)
+            .background(Color.green)
+            .clipShape(.capsule)
         }
     }
+    func generateQuestions () -> [ [Int] ] {
+        var retArr: [ [Int] ] = Array()
+        for _ in 1...(totalNumber+1)*5 {
+            retArr.append([Int.random(in: 0..<uptoNumber+1), Int.random(in: 0..<uptoNumber+1), 0])
+        }
+        return retArr
+    }
+
 }
 
 #Preview {
