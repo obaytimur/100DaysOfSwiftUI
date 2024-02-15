0f7201d3eeba07e58b899a5c7c7179e5b781fc80
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-15 13:57:16 +0300

    feat: add onDelete and editButton examples

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,35 +6,34 @@
 //
 
 import SwiftUI
-import Observation
-
-@Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
-}
-
-struct SecondView: View {
-    let name: String
-    @Environment(\.dismiss) var dismiss
-
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
 
 struct ContentView: View {
-    @State private var showingSheet = false
-    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Ogi")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
