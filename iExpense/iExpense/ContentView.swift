9d912cd216b8a20e4cbe0c21993911a9115d68bc
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-15 14:07:30 +0300

    feat: add appStorage example

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @AppStorage("tapCount") private var tapCount = 0
 
     var body: some View {
         Button("Tap count: \(tapCount)") {
             tapCount += 1
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
         }
     }
 }
