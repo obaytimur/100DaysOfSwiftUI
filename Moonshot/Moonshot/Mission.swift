e7c5d47b51c95241dfc0850bdf279cb2ab68bce9
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-20 11:30:59 +0300

    feat: update mission view

--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -12,9 +12,21 @@ struct Mission: Codable, Identifiable {
         let name: String
         let role: String
     }
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
 
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
 }
