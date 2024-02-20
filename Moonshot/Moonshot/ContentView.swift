98842bc7104a8f6a17edef646850d21760f04d7c
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-20 05:24:46 +0300

    feat: add JSON hieracrhy example

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { row in
-                NavigationLink("Row \(row)") {
-                    Text("Detail \(row)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .navigationTitle("SwiftUI")
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
     }
+}
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
 }
 
 #Preview {
