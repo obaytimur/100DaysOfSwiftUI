68bb59ec969c086ac4e3fcaccf98984beb003979
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-20 05:30:50 +0300

    feat: add lazyGrid example

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,36 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+    ]
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }
-}
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
 }
 
 #Preview {
