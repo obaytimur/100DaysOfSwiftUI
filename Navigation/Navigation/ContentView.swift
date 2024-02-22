0ed5e057f1adcb2f810cb4522849cab9b1e640f0
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-22 09:38:42 +0100

    feat: initialize navigation and add navigationDestination modifier

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Oğulcan Baytimur on 22/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Oğulcan Baytimur on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
