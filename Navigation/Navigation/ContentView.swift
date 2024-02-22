df32c2d439129f0a3aa7a3eeecf50470d1d926aa
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-22 09:56:11 +0100

    feat: add return to root path example

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -12,31 +12,26 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
                 }
-
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
-            .toolbar {
-                Button("Push 556") {
-                    path.append(556)
-                }
-
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
         }
+    }
+}
+
+struct DetailView: View {
+    var number: Int
+    @Binding var path: NavigationPath
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
     }
 }
 
