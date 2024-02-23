78d5d7872808a15f40e4fc0bad58cb41e5dbf4dd
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-23 15:14:49 +0100

    feat: add navigationBar variable name example

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -8,21 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title = "SwiftUI"
 
     var body: some View {
         NavigationStack {
             Text("Hello, world!")
-                .toolbar {
-                    ToolbarItemGroup(placement: .topBarLeading) {
-                        Button("Tap Me") {
-                            // button action here
-                        }
-
-                        Button("Tap Me 2") {
-                            // button action here
-                        }
-                    }
-                }
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
