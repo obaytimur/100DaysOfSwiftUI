d06cc2e198f957b8506c5cc5c5e407a3144581d2
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-26 19:23:52 +0100

    feat: initialize cupcakecorner and add networking example

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Oğulcan Baytimur on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
