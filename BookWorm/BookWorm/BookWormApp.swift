6d35b420985d2c8c932e2024594953774db6caf0
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-07 20:27:57 +0300

    feat: initialize bookWorm and add binding example

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Oğulcan Baytimur on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
