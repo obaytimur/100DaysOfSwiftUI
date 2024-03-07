4a1c26598fc2212b2fe95ff977f0cac6cbf96ff7
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-07 20:38:36 +0300

    feat: add textEditor example

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -8,36 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rememberMe = false
-    
+    @AppStorage("notes") private var notes = ""
+
     var body: some View {
-        VStack {
-            PushButton(title: "Remember Me", isOn: $rememberMe)
-            Text(rememberMe ? "On" : "Off")
+        NavigationStack {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
     }
 }
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom)
-        )
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
 #Preview {
     ContentView()
 }
