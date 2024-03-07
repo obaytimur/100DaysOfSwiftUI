7cce94940eabb4fc64314a9ea49896546ac1c464
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-07 23:06:33 +0300

    feat: add sortDescriptor

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -11,7 +11,10 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.rating, order: .reverse),
+        SortDescriptor(\Book.title)
+    ]) var books: [Book]
 
     @State private var showingAddScreen = false
 
