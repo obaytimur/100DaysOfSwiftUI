c7751cb0c364dc772ab148b0ff6e42deb3b63a96
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-09 23:55:53 +0300

    feat: add date and its picker to book

--- BookWorm/BookWorm/AddBookView.swift
+++ BookWorm/BookWorm/AddBookView.swift
@@ -16,6 +16,7 @@ struct AddBookView: View {
     @State private var rating = 3
     @State private var genre = "Fantasy"
     @State private var review = ""
+    @State private var date = Date()
     
     let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
     
@@ -31,16 +32,18 @@ struct AddBookView: View {
                             Text($0)
                         }
                     }
+                    DatePicker("Date:", selection: $date, displayedComponents: .date)
                 }
 
                 Section("Write a review") {
                     TextEditor(text: $review)
                     RatingView(rating: $rating)
                 }
+                
 
                 Section {
                     Button("Save") {
-                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating, date: date)
                         modelContext.insert(newBook)
                         dismiss()
                     }
--- BookWorm/BookWorm/DetailView.swift
+++ BookWorm/BookWorm/DetailView.swift
@@ -40,6 +40,8 @@ struct DetailView: View {
 
             RatingView(rating: .constant(book.rating))
                 .font(.largeTitle)
+            Text(String(book.date.formatted(date: .abbreviated, time: .omitted)))
+                .padding()
         }
         .alert("Delete book", isPresented: $showingDeleteAlert) {
             Button("Delete", role: .destructive, action: deleteBook)
@@ -67,7 +69,7 @@ struct DetailView: View {
     do {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Book.self, configurations: config)
-        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4, date: Date.now)
 
         return DetailView(book: example)
             .modelContainer(container)
