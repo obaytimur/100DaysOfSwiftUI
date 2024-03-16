643c1c2bdd638e1b880e2fd7d4b0ac4b809a2b20
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-03-16 17:09:58 +0300

    feat: initialize Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Oğulcan Baytimur on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
